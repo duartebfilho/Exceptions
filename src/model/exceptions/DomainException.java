316d4f2c3db0969e6581a17ffa16162fe10f06f4
Duarte Filho <duartebfilho@yahoo.com>
2023-06-07 13:04:47 +0200

    Excepton ex.1 v1

--- src/model/exceptions/DomainException.java
+++ src/model/exceptions/DomainException.java
@@ -7,4 +7,8 @@ public class DomainException extends Exception {
 	public DomainException(String msg) {
 		super(msg);
 	}
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 2eacd7f (Excepton ex.1 v1)
--- src/module-info.java
+++ src/module-info.java
@@ -5,5 +5,9 @@
  * @author meketreffe
  *
  */
+<<<<<<< HEAD
 module A174ExceptionsVeryBad {
+=======
+module Exception1 {
+>>>>>>> 2eacd7f (Excepton ex.1 v1)
 }
